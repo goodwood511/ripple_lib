ef2efbda8ba83a451fa253e1e18d1f604446b431
goodwood511 <goodwood511@gmail.com>
2025-07-04 16:07:19 +0800

    fix: not found balance will return zero balance;

--- ripple/xrpclient/xrpclient.go
+++ ripple/xrpclient/xrpclient.go
@@ -24,9 +24,33 @@ type accountInfoResponse struct {
 		AccountData struct {
 			Balance string `json:"Balance"`
 		} `json:"account_data"`
+		// 错误时存在这些字段
+		Error        string `json:"error,omitempty"`
+		ErrorCode    int    `json:"error_code,omitempty"`
+		ErrorMessage string `json:"error_message,omitempty"`
+		Status       string `json:"status,omitempty"` // "error" or "success"
 	} `json:"result"`
 }
 
+/*
+	{
+	    "result": {
+	        "account": "rKp7KgcYjdEQepQLc27ZHz76ukLwE4S1CN",
+	        "error": "actNotFound",
+	        "error_code": 19,
+	        "error_message": "Account not found.",
+	        "ledger_hash": "D59CDB76635F01A7BA054D8CFACAACA9D7ECF17C67ACB065CC55208DAEC18BF2",
+	        "ledger_index": 8629213,
+	        "request": {
+	            "account": "rKp7KgcYjdEQepQLc27ZHz76ukLwE4S1CN",
+	            "command": "account_info",
+	            "ledger_index": "validated"
+	        },
+	        "status": "error",
+	        "validated": true
+	    }
+	}
+*/
 func (c *Client) GetBalance(address string) (decimal.Decimal, error) {
 	req := map[string]interface{}{
 		"method": "account_info",
@@ -42,12 +66,23 @@ func (c *Client) GetBalance(address string) (decimal.Decimal, error) {
 	if err != nil {
 		return decimal.Zero, err
 	}
+	if res.Result.Error != "" {
+		if res.Result.ErrorCode == 19 {
+			return decimal.Zero, nil
+		}
+		return decimal.Zero, fmt.Errorf("%s: %s", res.Result.Error, res.Result.ErrorMessage)
+	}
 	return decimal.NewFromString(res.Result.AccountData.Balance)
 }
 
 type accountLinesResponse struct {
 	Result struct {
-		Lines []struct {
+		// 错误时存在这些字段
+		Error        string `json:"error,omitempty"`
+		ErrorCode    int    `json:"error_code,omitempty"`
+		ErrorMessage string `json:"error_message,omitempty"`
+		Status       string `json:"status,omitempty"` // "error" or "success"
+		Lines        []struct {
 			Currency string `json:"currency"`
 			Issuer   string `json:"account"`
 			Balance  string `json:"balance"`
